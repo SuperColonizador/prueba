17ccaf2041d40429d7e0466cfca5a4c8b42bfa06
SuperColonizador <supercolonizador@gmail.com>
2024-10-18 15:37:52 -0600

    Primer commit

---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Frm_Boton1 frm = new Frm_Boton1();
+            frm.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FrmBoton2 frm = new FrmBoton2();
+            frm.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FrmBoton3 frm = new FrmBoton3();
+            frm.Show();
+        }
+    }
+}
